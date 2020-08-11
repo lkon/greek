79cd0bf958a06c6dbeb03db4a977a93e1d329354
Konina Elena <yelenakonina@gmail.com>
2020-08-11 19:11:50 +0300

    stashed

--- get-file.js
+++ get-file.js
@@ -4,40 +4,40 @@ const Fs = require('fs');
 const Path = require('path');
 const Axios = require('axios');
 
-const sources = 'https://biblio-online.ru/viewer/getPage/7D324B09-E3A7-4637-9026-193EBCCA7A2C/';
-let index = 107;
+const sources = page => `https://www.idesk24.com/LMS_LIGHT/BOOKS/bk39616431/jpg/pg_${page}.jpg?740`;
+let index = 1;
 
-const srcLength = 108;
-// const srcLength = 200;
+const srcLength = 148;
 
 async function downloadFile(i) {
-    const url = `${sources}${i}`;
-    const fileName = `page_${index}.svg`;
+    const url = sources(i);
+    const fileName = `page_${index}.jpg`;
 
     console.log(url, fileName)
 
     const path = Path.resolve(__dirname, 'files', fileName);
-    // const writer = Fs.writeFile(path);
+    const writer = Fs.createWriteStream(path);
 
     const response = await Axios({
         url,
         method: 'GET',
-        responseType: 'document'
+        responseType: 'stream'
     });
 
-    console.log(response)
-
-    // response.data.pipe(writer);
+    response.data.pipe(writer);
 
     return new Promise((resolve, reject) => {
-        // writer.on('finish', resolve);
-        // writer.on('error', reject);
+        writer.on('finish', resolve);
+        writer.on('error', reject);
 
-        resolve(); reject();
+        // resolve(); reject();
     }).then(() => {
-        // index++;
-        // index <= srcLength && downloadFile(index);
+        index++;
+        index <= srcLength && downloadFile(index);
     });
 }
 
 downloadFile(index);
+
+
+
